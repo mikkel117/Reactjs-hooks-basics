9a9939f3acde5623b41afbf211cf1c97a378f8a1
mikkel117 <mikkel@legbilligt.dk>
2020-07-16 00:15:24 +0200

    update
    a link to a sight that explain more about array

--- src/component/page/array.jsx
+++ src/component/page/array.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
-
 const names = ['James', 'Paul', 'John', 'George', 'Ringo'];
 
-export default function Array(){
-    return(
-        <div>
-            {names.map(name => 
-                <p>
-                    {name}
-                </p>
-                )}
-        </div>
-    )
+export default function Array() {
+  return (
+    <section>
+      <div>
+        {names.map((name) => (
+          <p>{name}</p>
+        ))}
+      </div>
+      <br/>
+      <p>go to this site to lorn <a href="https://upmostly.com/tutorials/how-to-for-loop-in-react-with-examples" target="blank">more</a></p>
+    </section>
+  );
 }

8c7586b088e225087c96def82f9c73e581fd28e2
mikkel117 <mikkel@legbilligt.dk>
2020-08-03 08:16:49 +0200

    update

--- src/component/page/form.jsx
+++ src/component/page/form.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 
 export default function Form() {
   const [buttonText, setButtonText] = useState('Click me, please');
